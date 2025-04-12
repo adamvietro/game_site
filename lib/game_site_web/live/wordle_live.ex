d675f495ed46bd41274bfd03a7eec304275f1d33
adam vietro <hunterafi@gmail.com>
2025-04-11 18:24:49 -0700

    Did some more work on the Wordle game and added in some more functionality. I still have more to work on in regards to the game (see todo). I want to take a min and fine tune a few things before I deploy. Fix the main page to have a way of getting to every page, Add in a username for each new user, set the header for each page, take away the ability for a player to see the games index as I will be adding in those as needed with the fly.io shell comands.

--- lib/game_site_web/live/wordle_live.ex
+++ lib/game_site_web/live/wordle_live.ex
@@ -29,20 +29,37 @@ defmodule GameSiteWeb.WordleLive do
     21 => "bg-gray-100",
     22 => "bg-gray-100",
     23 => "bg-gray-100",
-    24 => "bg-gray-100"
+    24 => "bg-gray-100",
+    25 => "bg-gray-100",
+    26 => "bg-gray-100",
+    27 => "bg-gray-100",
+    28 => "bg-gray-100",
+    29 => "bg-gray-100"
+  }
+
+  @starting_entries %{
+    first: %{l1: "1", l2: "1", l3: "1", l4: "1", l5: "1"},
+    second: %{l1: "2", l2: "2", l3: "2", l4: "2", l5: "2"},
+    third: %{l1: "3", l2: "3", l3: "3", l4: "3", l5: "3"},
+    fourth: %{l1: "4", l2: "4", l3: "4", l4: "4", l5: "4"},
+    fifth: %{l1: "5", l2: "5", l3: "5", l4: "5", l5: "5"},
+    sixth: %{l1: "6", l2: "6", l3: "6", l4: "6", l5: "6"}
   }
 
   def render(assigns) do
     ~H"""
     <p>Score: {@score}</p>
-    <p>Word: {@word}</p>
+    <%= if @reset == true do %>
+      <p>Word: {@word}</p>
+    <% end %>
     <div class="grid grid-cols-5 gap-4">
       <%= for {label, index} <- Enum.with_index([
-    "01", "01", "01", "01", "15",
-    "02", "02", "02", "02", "02",
-    "03", "03", "03", "03", "03",
-    "04", "04", "04", "04", "04",
-    "51", "05", "05", "05", "05"
+    @entry.first.l1 , @entry.first.l2, @entry.first.l3, @entry.first.l4, @entry.first.l5,
+    @entry.second.l1, @entry.second.l2, @entry.second.l3, @entry.second.l4, @entry.second.l5,
+    @entry.third.l1 , @entry.third.l2, @entry.third.l3, @entry.third.l4, @entry.third.l5,
+    @entry.fourth.l1, @entry.fourth.l2, @entry.fourth.l3, @entry.fourth.l4, @entry.fourth.l5,
+    @entry.fifth.l1 , @entry.fifth.l2, @entry.fifth.l3, @entry.fifth.l4, @entry.fifth.l5,
+    @entry.sixth.l1 , @entry.sixth.l2, @entry.sixth.l3, @entry.sixth.l4, @entry.sixth.l5
     ]) do %>
         <div class={"p-4 text-center rounded " <> Map.get(@state, index)}>
           {label}
@@ -61,7 +78,13 @@ defmodule GameSiteWeb.WordleLive do
     <%= if @reset == false do %>
       <div class="user-input">
         <.simple_form id="input-form" for={@form} phx-submit="guess">
-          <.input type="text" field={@form[:guess]} label="Guess" />
+          <.input
+            type="text"
+            field={@form[:guess]}
+            label="Guess"
+            value={@form[:guess].value}
+            key={@form}
+          />
           <:actions>
             <.button class="px-6 py-2 text-lg">Submit</.button>
           </:actions>
@@ -76,7 +99,10 @@ defmodule GameSiteWeb.WordleLive do
         it's not in the right place. A grey box means that the letter isn't even in the word. The faster
         (number of guesses) the higher score you will receive.
         <br />Please enjoy below is a list of things I want to add. <br />#todo:
-        <br />add a wager button (for more points)
+        <br />add a wager button (for more points) <br />Add a sixth row
+        <br />Add a keyboard with colors <br />Add a streak counter
+        <br />Add in an entry socket to keep track of the entered words
+        <br />Add check for max characters
       </div>
     </body>
     <.simple_form id="exit-form" for={@form} phx-submit="exit">
@@ -96,10 +122,11 @@ defmodule GameSiteWeb.WordleLive do
        socket,
        state: @starting_state,
        score: 0,
-       form: to_form(%{}),
+       form: to_form(%{guess: ""}),
        word: Words.get_word(),
        round: 0,
-       reset: false
+       reset: false,
+       entry: @starting_entries
      )}
   end
 
@@ -107,9 +134,12 @@ defmodule GameSiteWeb.WordleLive do
     colors = feedback(socket.assigns.word, params["guess"])
     state = set_colors(colors, socket.assigns.round, socket.assigns.state)
 
+    form_data = %{"guess" => ""}
+    new_form = to_form(form_data, errors: [guess: {"incorrect", []}])
+
     cond do
       correct?(colors) and
-          socket.assigns.round <= 4 ->
+          socket.assigns.round <= 5 ->
         score = (5 - socket.assigns.round) * 10
 
         {:noreply,
@@ -118,30 +148,35 @@ defmodule GameSiteWeb.WordleLive do
            round: 0,
            score: socket.assigns.score + score,
            state: state,
-           reset: true
+           reset: true,
+           form: to_form(%{"guess" => ""}),
+           entry: entries(socket.assigns.entry, params["guess"], socket.assigns.round)
          )}
 
-      socket.assigns.round < 4 ->
+      socket.assigns.round < 5 ->
         {:noreply,
          assign(
            socket,
+           entry: entries(socket.assigns.entry, params["guess"], socket.assigns.round),
            round: socket.assigns.round + 1,
+           form: new_form,
            state: state
          )}
 
-      socket.assigns.round == 4 ->
+      socket.assigns.round == 5 ->
         {:noreply,
          assign(
            socket,
            round: 0,
-           word: Words.get_word(),
            state: state,
-           reset: true
+           form: new_form,
+           reset: true,
+           entry: entries(socket.assigns.entry, params["guess"], socket.assigns.round)
          )}
     end
   end
 
-  def handle_event("reset", params, socket) do
+  def handle_event("reset", _params, socket) do
     {:noreply,
      assign(
        socket,
@@ -149,8 +184,89 @@ defmodule GameSiteWeb.WordleLive do
        state: @starting_state,
        word: Words.get_word(),
        reset: false,
-       form: to_form(%{})
+       form: to_form(%{guess: ""}),
+       entry: @starting_entries
      )}
+  end
+
+  defp entries(entries, word, round) do
+    word =
+      word
+      |> String.split("", trim: true)
+
+    case round do
+      0 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.first.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.first.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.first.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.first.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        entries = put_in(entries.first.l5, letter)
+
+      1 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.second.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.second.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.second.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.second.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        _entries = put_in(entries.second.l5, letter)
+
+      2 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.third.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.third.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.third.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.third.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        _entries = put_in(entries.third.l5, letter)
+
+      3 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.fourth.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.fourth.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.fourth.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.fourth.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        _entries = put_in(entries.fourth.l5, letter)
+
+      4 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.fifth.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.fifth.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.fifth.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.fifth.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        _entries = put_in(entries.fifth.l5, letter)
+
+      5 ->
+        {letter, _list} = List.pop_at(word, 0, "")
+        entries = put_in(entries.sixth.l1, letter)
+        {letter, _list} = List.pop_at(word, 1, "")
+        entries = put_in(entries.sixth.l2, letter)
+        {letter, _list} = List.pop_at(word, 2, "")
+        entries = put_in(entries.sixth.l3, letter)
+        {letter, _list} = List.pop_at(word, 3, "")
+        entries = put_in(entries.sixth.l4, letter)
+        {letter, _list} = List.pop_at(word, 4, "")
+        _entries = put_in(entries.sixth.l5, letter)
+    end
   end
 
   defp feedback(word, guess) do
@@ -161,6 +277,7 @@ defmodule GameSiteWeb.WordleLive do
 
     index_guess =
       guess
+      |> String.downcase()
       |> String.split("", trim: true)
       |> Enum.with_index()
 
@@ -185,7 +302,7 @@ defmodule GameSiteWeb.WordleLive do
         {color, _list} = List.pop_at(colors, 3, :gray)
         state = put_in(state[3], color)
         {color, _list} = List.pop_at(colors, 4, :gray)
-        state = put_in(state[4], color)
+        _state = put_in(state[4], color)
 
       1 ->
         {color, _list} = List.pop_at(colors, 0, :gray)
@@ -197,7 +314,7 @@ defmodule GameSiteWeb.WordleLive do
         {color, _list} = List.pop_at(colors, 3, :gray)
         state = put_in(state[8], color)
         {color, _list} = List.pop_at(colors, 4, :gray)
-        state = put_in(state[9], color)
+        _state = put_in(state[9], color)
 
       2 ->
         {color, _list} = List.pop_at(colors, 0, :gray)
@@ -209,7 +326,7 @@ defmodule GameSiteWeb.WordleLive do
         {color, _list} = List.pop_at(colors, 3, :gray)
         state = put_in(state[13], color)
         {color, _list} = List.pop_at(colors, 4, :gray)
-        state = put_in(state[14], color)
+        _state = put_in(state[14], color)
 
       3 ->
         {color, _list} = List.pop_at(colors, 0, :gray)
@@ -221,7 +338,7 @@ defmodule GameSiteWeb.WordleLive do
         {color, _list} = List.pop_at(colors, 3, :gray)
         state = put_in(state[18], color)
         {color, _list} = List.pop_at(colors, 4, :gray)
-        state = put_in(state[19], color)
+        _state = put_in(state[19], color)
 
       4 ->
         {color, _list} = List.pop_at(colors, 0, :gray)
@@ -233,7 +350,19 @@ defmodule GameSiteWeb.WordleLive do
         {color, _list} = List.pop_at(colors, 3, :gray)
         state = put_in(state[23], color)
         {color, _list} = List.pop_at(colors, 4, :gray)
-        state = put_in(state[24], color)
+        _state = put_in(state[24], color)
+
+      5 ->
+        {color, _list} = List.pop_at(colors, 0, :gray)
+        state = put_in(state[25], color)
+        {color, _list} = List.pop_at(colors, 1, :gray)
+        state = put_in(state[26], color)
+        {color, _list} = List.pop_at(colors, 2, :gray)
+        state = put_in(state[27], color)
+        {color, _list} = List.pop_at(colors, 3, :gray)
+        state = put_in(state[28], color)
+        {color, _list} = List.pop_at(colors, 4, :gray)
+        _state = put_in(state[29], color)
     end
   end
 
