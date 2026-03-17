8a6b822091995a4b49287f4a93020353b08a63e1
adam vietro <hunterafi@gmail.com>
2026-03-16 17:24:57 -0700

    poker_form.ex Removed all need for PokerForm as it was not needed after I added in the game_logic struct.
    Now that is in I was able to leverage the struct for GameLogic to hold all the needed information about the game.

--- lib/game_site_web/live/poker_live.ex
+++ lib/game_site_web/live/poker_live.ex
@@ -3,29 +3,26 @@ defmodule GameSiteWeb.PokerLive do
 
   alias GameSiteWeb.Live.Component, as: LiveComponent
   alias GameSiteWeb.Live.PokerLive.{Component, GameBoard, GameLogic}
-  alias GameSiteWeb.PokerForm
-  alias GameSiteWeb.PokerLive.PokerHelpers, as: Helper
   alias GameSite.Scores.ScoreHandler
 
-  @all_in_list ["initial", "dealt", "final"]
-
+  @impl true
   def render(assigns) do
     ~H"""
     <div class="max-w-4xl mx-auto p-4 space-y-8">
       <Component.instructions />
       <LiveComponent.score_board
         highest_score={@highest_score}
-        current_score={@score}
-        current_bet={@wager}
+        current_score={@game.score}
+        current_bet={@game.wager}
       />
 
       <GameBoard.game_board
         form={@form}
-        hand={@hand}
-        score={@score}
-        wager={@wager}
-        state={@state}
-        all_in={@all_in}
+        hand={@game.hand}
+        score={@game.score}
+        wager={@game.wager}
+        state={@game.state}
+        all_in={@game.all_in}
       />
       <Component.rules />
 
@@ -39,165 +36,83 @@ defmodule GameSiteWeb.PokerLive do
     """
   end
 
-  def mount(_params, _session, socket) do
-    default = PokerForm.default_values()
+  @impl true
 
+  def mount(_params, _session, socket) do
     socket =
       socket
-      |> assign(default)
+      |> assign(game: GameLogic.new_game())
+      |> assign(form: %{})
+      |> assign(highest_score: 0)
 
     {:ok, socket}
   end
 
-  def handle_event("deal-cards", params, socket) do
-    {hand, cards} =
-      Helper.cards()
-      |> Helper.shuffle()
-      |> Helper.choose_5()
+  @impl true
+  def handle_event("advance", params, socket) do
+    advanced_game = GameLogic.advance_game(socket.assigns.game, params)
 
-    wager =
-      if socket.assigns.all_in do
-        socket.assigns.wager
-      else
-        String.to_integer(params["wager"])
-      end
+    socket =
+      socket
+      |> assign(game: advanced_game)
+      |> assign(highest_score: max(advanced_game.score, socket.assigns.highest_score))
 
-    {:ok, valid} =
-      PokerForm.merge_assigns(socket.assigns, %{
-        cards: cards,
-        hand: hand,
-        state: "dealt",
-        wager: wager
-      })
-
-    {:noreply, assign(socket, valid)}
+    {:noreply, socket}
   end
 
-  def handle_event("new-hand", _params, socket) do
-    {:ok, valid} =
-      PokerForm.merge_assigns(socket.assigns, %{
+  @impl true
+  def handle_event("new-hand", _params, %{assigns: %{game: game}} = socket) do
+    valid =
+      %{
         hand: [],
         cards: [],
         state: "initial",
         all_in: false
-      })
+      }
 
-    {:noreply, assign(socket, valid)}
+    {:noreply, assign(socket, game: GameLogic.modify_game(game, valid))}
   end
 
-  def handle_event("advance", params, socket) do
-    case socket.assigns.state do
-      "initial" ->
-        merge_map = GameLogic.advance_game(socket.assigns.state)
-
-        wager = parse_wager(params)
-        merge_map = Map.put(merge_map, :wager, wager)
-
-        {:ok, valid} =
-          PokerForm.merge_assigns(socket.assigns, merge_map)
-
-        {:noreply, assign(socket, valid)}
-
-      "dealt" ->
-        # selected_cards = selected_cards(Map.get(params, "replace", []))
-        [new_hand, cards] = new_hand(params, socket)
-
-        {:ok, valid} =
-          PokerForm.merge_assigns(socket.assigns, %{
-            hand: new_hand,
-            cards: cards,
-            state: "final"
-          })
-
-        {:noreply, assign(socket, valid)}
-
-      "final" ->
-        {:ok, valid} =
-          case Helper.classify(socket.assigns.hand) do
-            {:high_card, _rank} ->
-              score = socket.assigns.score - socket.assigns.wager
-
-              PokerForm.merge_assigns(
-                socket.assigns,
-                %{
-                  state: "reset",
-                  all_in: false,
-                  score: score,
-                  wager: min(score, socket.assigns.wager)
-                }
-              )
-
-            {:one_pair, [rank]} ->
-              if rank >= 11 do
-                PokerForm.merge_assigns(
-                  socket.assigns,
-                  %{
-                    score: socket.assigns.score + socket.assigns.wager,
-                    state: "reset",
-                    all_in: false
-                  }
-                )
-              else
-                score = socket.assigns.score - socket.assigns.wager
-
-                PokerForm.merge_assigns(
-                  socket.assigns,
-                  %{
-                    state: "reset",
-                    all_in: false,
-                    score: score,
-                    wager: min(score, socket.assigns.wager)
-                  }
-                )
-              end
-
-            {_hand, _rank_suit} ->
-              score = socket.assigns.score + socket.assigns.wager
-
-              PokerForm.merge_assigns(
-                socket.assigns,
-                %{
-                  score: score,
-                  state: "reset",
-                  all_in: false
-                }
-              )
-          end
-
-        {:noreply, assign(socket, valid)}
-
-      "reset" ->
-        {:ok, valid} =
-          PokerForm.merge_assigns(socket.assigns, %{
-            state: "initial",
-            all_in: false
-          })
-
-        {:noreply, assign(socket, valid)}
-    end
-  end
-
-  def handle_event("all-in", _params, socket) do
+  @impl true
+  def handle_event("reset", _params, %{assigns: %{game: game}} = socket) do
     valid = %{
-      bet: socket.assigns.score,
-      all_in: true,
-      wager: socket.assigns.score
+      score: 100,
+      bet: 0,
+      hand: [],
+      cards: [],
+      wager: 10,
+      state: "initial",
+      all_in: false
     }
 
-    socket = assign(socket, valid)
-    IO.inspect(socket.assigns, label: "After All-In")
+    socket = assign(socket, game: GameLogic.modify_game(game, valid))
 
     {:noreply, socket}
   end
 
+  @impl true
+  def handle_event("all-in", _params, %{assigns: %{game: game}} = socket) do
+    valid = %{
+      all_in: true,
+      wager: game.score
+    }
+
+    socket = assign(socket, game: GameLogic.modify_game(game, valid))
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def handle_event("increase_wager", _params, %{assigns: %{all_in: true}} = socket) do
     {:noreply, socket}
   end
 
+  @impl true
   def handle_event("decrease_wager", _params, %{assigns: %{all_in: true}} = socket) do
     {:noreply, socket}
   end
 
+  @impl true
   def handle_event("increase_wager", _params, socket) do
     new_wager = min(socket.assigns.wager + 10, socket.assigns.score)
 
@@ -206,6 +121,7 @@ defmodule GameSiteWeb.PokerLive do
     {:noreply, assign(socket, wager: new_wager, all_in: all_in)}
   end
 
+  @impl true
   def handle_event("decrease_wager", _params, socket) do
     new_wager = max(socket.assigns.wager - 10, 10)
 
@@ -214,57 +130,8 @@ defmodule GameSiteWeb.PokerLive do
     {:noreply, assign(socket, wager: new_wager, all_in: all_in)}
   end
 
-  def handle_event("reset", _params, socket) do
-    {:ok, reset_assigns} =
-      PokerForm.merge_assigns(socket.assigns, %{
-        score: 100,
-        bet: 0,
-        hand: [],
-        cards: [],
-        wager: 10,
-        state: "initial",
-        all_in: false
-      })
-
-    {:noreply, assign(socket, reset_assigns)}
-  end
-
+  @impl true
   def handle_event("exit", params, socket) do
     ScoreHandler.save_score(socket, params)
-  end
-
-  defp parse_wager(params) do
-    if params["all_in"] == "true" do
-      params["score"]
-    else
-      String.to_integer(params["wager"])
-    end
-  end
-
-  defp new_hand(params, socket) do
-    raw_cards =
-      Map.get(params, "replace", [])
-
-    number_of_cards =
-      length(raw_cards)
-
-    selected_cards = selected_cards(raw_cards)
-
-    new_hand = Helper.remove_cards(selected_cards, socket.assigns.hand)
-    Helper.choose(socket.assigns.cards, new_hand, number_of_cards)
-  end
-
-  defp selected_cards([]), do: []
-
-  defp selected_cards(raw_cards) do
-    Enum.map(raw_cards, fn string ->
-      [rank, suit] = String.split(string, ":")
-      {String.to_integer(rank), suit}
-    end)
-  end
-
-  def parse_card_param(param) do
-    [rank, suit] = String.split(param, ":")
-    {rank, suit}
   end
 end
--- lib/game_site_web/live/poker_live/game_logic.ex
+++ lib/game_site_web/live/poker_live/game_logic.ex
@@ -17,50 +17,59 @@ defmodule GameSiteWeb.Live.PokerLive.GameLogic do
     struct(game, changes)
   end
 
-  def advance_game(%__MODULE__{} = game) do
-    case game.state do
-      "initial" ->
-        {hand, cards} = deal_cards()
+  def advance_game(%__MODULE__{state: "initial"} = game, _params) do
+    {hand, cards} = deal_cards()
 
-        %{game | hand: hand, cards: cards, state: "dealt"}
+    %{game | hand: hand, cards: cards, state: "dealt"}
+  end
 
-      "dealt" ->
-        [new_hand, cards] = update_hand(game.selected_cards, game.hand, game.cards)
+  def advance_game(%__MODULE__{state: "dealt"} = game, params) do
+    raw_cards = Map.get(params, "replace", [])
+    [new_hand, cards] = update_hand(selected_cards(raw_cards), game.hand, game.cards)
 
-        %{game | hand: new_hand, cards: cards, state: "final"}
+    %{game | hand: new_hand, cards: cards, state: "final"}
+  end
 
-      "final" ->
-        case Helper.classify(game.hand) do
-          {:high_card, _rank} ->
-            score = game.score - game.wager
+  def advance_game(%__MODULE__{state: "final"} = game, _params) do
+    case Helper.classify(game.hand) do
+      {:high_card, _rank} ->
+        score = game.score - game.wager
 
-            %{game | state: "reset", all_in: false, score: score, wager: min(score, game.wager)}
+        %{game | state: "reset", all_in: false, score: score, wager: min(score, game.wager)}
 
-          {:one_pair, [rank]} ->
-            if rank >= 11 do
-              %{game | score: game.score + game.wager, state: "reset", all_in: false}
-            else
-              score = game.score - game.wager
+      {:one_pair, [rank]} ->
+        if rank >= 11 do
+          %{game | score: game.score + game.wager, state: "reset", all_in: false}
+        else
+          score = game.score - game.wager
 
-              %{
-                game
-                | state: "reset",
-                  all_in: false,
-                  score: score,
-                  wager: min(score, game.wager)
-              }
-            end
-
-          {_hand, _rank_suit} ->
-            score = game.score + game.wager
-
-            %{game | score: score, state: "reset", all_in: false}
+          %{
+            game
+            | state: "reset",
+              all_in: false,
+              score: score,
+              wager: min(score, game.wager)
+          }
         end
 
-      "reset" ->
-        %{game | state: "initial", all_in: false}
-    end
+      {_hand, _rank_suit} ->
+        score = game.score + game.wager
 
+        %{game | score: score, state: "reset", all_in: false}
+    end
+  end
+
+  def advance_game(%__MODULE__{state: "reset"} = game, _params) do
+    %{game | state: "initial", all_in: false}
+  end
+
+  defp selected_cards([]), do: []
+
+  defp selected_cards(raw_cards) do
+    Enum.map(raw_cards, fn string ->
+      [rank, suit] = String.split(string, ":")
+      {String.to_integer(rank), suit}
+    end)
   end
 
   def deal_cards() do
