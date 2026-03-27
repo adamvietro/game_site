f88c7655c1cbe999ab550b9f64d03579797963c2
adam vietro <hunterafi@gmail.com>
2026-03-26 19:12:29 -0700

    update_player Added functionality for updating a player withing the Room context.

--- lib/game_site/multi_poker/room.ex
+++ lib/game_site/multi_poker/room.ex
@@ -24,22 +24,6 @@ defmodule GameSite.MultiPoker.Room do
     }
   end
 
-  def add_player(pid, %Player{} = player) do
-    GenServer.cast(pid, {:add_player, player})
-  end
-
-  def remove_player(pid, %Player{} = player) do
-    GenServer.cast(pid, {:remove_player, player})
-  end
-
-  def update_status(pid, status) do
-    GenServer.cast(pid, {:update_status, status})
-  end
-
-  def get_state(pid) do
-    GenServer.call(pid, :get_room_state)
-  end
-
   def start_link(host, opts \\ []) do
     room_id = Keyword.fetch!(opts, :room_id)
 
@@ -48,6 +32,26 @@ defmodule GameSite.MultiPoker.Room do
       {host, opts},
       name: via(room_id)
     )
+  end
+
+  def add_player(pid, %Player{} = player) do
+    GenServer.cast(pid, {:add_player, player})
+  end
+
+  def remove_player(pid, %Player{} = player) do
+    GenServer.cast(pid, {:remove_player, player})
+  end
+
+  def update_player(pid, player_id, opts) do
+    GenServer.cast(pid, {:update_player, player_id, opts})
+  end
+
+  def update_status(pid, status) do
+    GenServer.cast(pid, {:update_status, status})
+  end
+
+  def get_state(pid) do
+    GenServer.call(pid, :get_room_state)
   end
 
   @impl true
@@ -70,6 +74,21 @@ defmodule GameSite.MultiPoker.Room do
   @impl true
   def handle_cast({:remove_player, %Player{} = player}, %__MODULE__{players: players} = state) do
     new_players = Map.delete(players, player.player_id)
+    {:noreply, %__MODULE__{state | players: new_players}}
+  end
+
+  @impl true
+  def handle_cast({:update_player, player_id, opts}, %__MODULE__{players: players} = state) do
+    new_players =
+      case Map.fetch(players, player_id) do
+        {:ok, player} ->
+          updated_player = Player.change(player, opts)
+          Map.put(players, player_id, updated_player)
+
+        :error ->
+          players
+      end
+
     {:noreply, %__MODULE__{state | players: new_players}}
   end
 
