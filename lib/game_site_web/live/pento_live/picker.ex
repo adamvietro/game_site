d5d3ef51fae3d0cc2f8bd34d5cc3521e8700d23c
adam vietro <hunterafi@gmail.com>
2026-04-09 21:06:30 -0700

    Pento Picker Better looking Picker for the Pento Game.

--- lib/game_site_web/live/pento_live/picker.ex
+++ lib/game_site_web/live/pento_live/picker.ex
@@ -21,29 +21,37 @@ defmodule GameSiteWeb.PentoLive.Picker do
     <h1 class="font-heavy text-4xl text-center mb-6">
       Choose a Puzzle
     </h1>
-    <%= for {puzzle, board} <- @boards do %>
-      <.row board={board} puzzle={puzzle} />
-    <% end %>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+      <%= for {puzzle, board} <- @boards do %>
+        <.row board={board} puzzle={puzzle} />
+      <% end %>
+    </div>
     """
   end
-
-  attr(:board, :any, required: true)
-  attr(:puzzle, :atom, required: true)
 
   def row(assigns) do
     ~H"""
     <.link navigate={~p"/pento/#{@puzzle}"}>
-      <div class="grid grid-cols-2 hover:bg-slate-200">
-        <div>
-          <h3 class="text-2xl">Pieces</h3>
+      <div class="grid grid-cols-2 gap-4 p-4
+            divide-x divide-slate-300
+            border-2 border-slate-300
+            rounded-xl bg-gray-100
+            shadow-sm hover:shadow-lg
+            hover:bg-gray-200
+            transform hover:-translate-y-0.5
+            transition-all duration-200 cursor-pointer">
+        <div class="pr-4 space-y-2">
+          <h3 class="text-xl sm:text-2xl">Pieces</h3>
+          <.palette shape_names={@board.palette} />
         </div>
-        <div>
-          <h3 class="text-2xl">
-            {@puzzle |> to_string |> String.capitalize()} Puzzle
+
+        <div class="pl-4 space-y-2">
+          <h3 class="text-xl sm:text-2xl">
+            {@puzzle |> to_string() |> String.capitalize()} Puzzle
           </h3>
+          <.board board={@board} />
         </div>
-        <.palette shape_names={@board.palette} />
-        <.board board={@board} />
       </div>
     </.link>
     """
